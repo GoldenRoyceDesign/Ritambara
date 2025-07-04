e1746bbea5ee43d83493989cab3633740022cf64
unknown <divyaneela75@gmail.com>
2025-07-04 11:46:50 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,52 +2,71 @@
 import { Container, Navbar, Nav, Offcanvas } from 'react-bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import logo from '../assets/logo.png';
+import { useState } from 'react';
 
 const NavbarComponent = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // Scroll manually after closing offcanvas
+  const handleNavClick = (e, sectionId) => {
+    e.preventDefault();
+    setShow(false); // Close offcanvas
+
+    // Delay scroll until offcanvas animation is complete
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 300); // Delay (matches Bootstrap offcanvas transition)
+  };
+
   return (
-    <>
-      {/* Supports offcanvas for mobile */}
-      <Navbar key="lg" expand="lg" className="navbar pe-3 ps-3 pt-0 pb-0 m-0">
-        <Container fluid>
-          {/* Logo */}
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              width="80"
-              height="80"
-              className="d-inline-block align-top"
-              alt="Logo"
-            />
-          </Navbar.Brand>
+    <Navbar expand="lg" className="navbar pe-3 ps-3 pt-0 pb-0 m-0">
+      <Container fluid>
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            width="80"
+            height="80"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+        </Navbar.Brand>
 
-          {/* Toggle for Offcanvas */}
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+        <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
 
-          {/* Offcanvas */}
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-lg`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-            placement="end" // slide from right
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-                
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-
-            <Offcanvas.Body>
-              <Nav className="justify-content-center gap-4 ms-auto">
-                <Nav.Link href="#home" className="navlink">HOME</Nav.Link>
-                <Nav.Link href="#about" className="navlink">ABOUT US</Nav.Link>
-                <Nav.Link href="#service" className="navlink">SERVICE</Nav.Link>
-                <Nav.Link href="#contact" className="navlink">CONTACT US</Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </>
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+          show={show}
+          onHide={handleClose}
+        >
+          <Offcanvas.Header closeButton />
+          <Offcanvas.Body>
+            <Nav className="justify-content-center gap-4 ms-auto">
+              <Nav.Link as="button" className="navlink btn btn-link p-0 text-start" onClick={(e) => handleNavClick(e, 'home')}>
+                HOME
+              </Nav.Link>
+              <Nav.Link as="button" className="navlink btn btn-link p-0 text-start" onClick={(e) => handleNavClick(e, 'about')}>
+                ABOUT US
+              </Nav.Link>
+              <Nav.Link as="button" className="navlink btn btn-link p-0 text-start" onClick={(e) => handleNavClick(e, 'service')}>
+                SERVICE
+              </Nav.Link>
+              <Nav.Link as="button" className="navlink btn btn-link p-0 text-start" onClick={(e) => handleNavClick(e, 'contact')}>
+                CONTACT US
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
-};
+}
 
 export default NavbarComponent;
